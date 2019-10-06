7b913ae90f59acce3782f3e7745d7011909c5af7
yuikotonoha <harutinkudo042@gmail.com>
2019-10-06 18:07:15 +0900

    [add]ログインしてないユーザーへのアクセス制限※sorceryのメソッド

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,11 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
   def new; end
 
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, success: 'ログインしました'
+      redirect_back_or_to boards_path, success: 'ログインしました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
